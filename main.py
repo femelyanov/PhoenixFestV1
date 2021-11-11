a39a18d838c85ea7978e3707f9538df5ac59acd9
Fillip Emelyanov <fillip.emelyanov@gmail.com>
2021-11-11 10:50:21 -0500

    sorted the preferences

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ all_students = pd.read_csv("All Students.csv")
 preference_responses = pd.read_csv("Preference Responses.csv")
 program_data = pd.read_csv("Program Data.csv")
 presenters = pd.read_csv("Presenters.csv")
-
+'''
 print("All Students")
 print(all_students.head())
 print(all_students.dtypes)
@@ -20,3 +20,7 @@ print(program_data.dtypes)
 print("\n Presenters")
 print(presenters.head())
 print(presenters.dtypes)
+'''
+pd.set_option('display.max_rows', None)
+pr = preference_responses.sort_values(by=['What grade are you in?'])
+pr.to_csv("Preference Responses.csv", index=False)
