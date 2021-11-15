068b41f77abe72e7fe76c06241be94a04b389b08
Fillip Emelyanov <fillip.emelyanov@gmail.com>
2021-11-15 18:47:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from pandas.io.pytables import dropna_doc
 
+#reads all the files given
 all_students = pd.read_csv("All Students.csv")
 preference_responses = pd.read_csv("Preference Responses.csv")
 program_data = pd.read_csv("Program Data.csv")
 presenters = pd.read_csv("Presenters.csv")
-student_choices = {}
+student_choices = {} #creates a dictionary to store the student assignments
+
+#prints all the dataframes and their types
 '''
 print("All Students")
 print(all_students.head())
@@ -23,11 +26,13 @@ print("\n \n Presenters")
 print(presenters.head())
 print(presenters.dtypes)
 '''
-pd.set_option('display.max_rows', None)
+
+#sorts the preference responses by grade level (9->12) and saves (to the same file)
 pr = preference_responses.sort_values(by=['What grade are you in?'])
 pr.to_csv("Preference Responses.csv", index=False)
 
-#make all values in column lowercase
+#formats the data and gets the final list of just the students that did not sign up
+#make all values in 'Email'column in all_students lowercase
 for column in all_students.columns:
     all_students['Email'] = all_students['Email'].str.lower()
 #make all vlaues in 'Email' column in presenters lowercase
@@ -37,9 +42,6 @@ for column in presenters.columns:
 all_students = all_students[~all_students['Email'].isin(presenters['Email'])]
 #save all_students to a new csv file
 all_students.to_csv("All Students.csv", index=False)
-#check if values of 'Email' column in 'all_students' are in 'presenters'
-print(all_students['Email'].isin(presenters['Email']))
-
 #make all values in "Email" column in 'preference responses' lowercase
 for column in preference_responses.columns:
     preference_responses['Email'] = preference_responses['Email'].str.lower()
@@ -47,3 +49,5 @@ for column in preference_responses.columns:
 all_students = all_students[~all_students['Email'].isin(preference_responses['Email'])]
 #save all_students to a new csv file
 all_students.to_csv("All Students1.csv", index=False)
+
+#
