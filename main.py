31af286279a2eac5255dacc7b4c03e16e02d638e
Fillip Emelyanov <fillip.emelyanov@gmail.com>
2022-03-22 23:15:15 -0400

    put in comments as to what is happening

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import pprint
+from hungarian_algorithm import algorithm
+import random
 
 from program import Program
 from student import Student
@@ -58,7 +60,7 @@ for column in presenters.columns:
 #all_students = all_students[~all_students['Email'].isin(preference_responses['Email'])]
 #save all_students to a new csv file
 #all_students.to_csv("No Choices.csv", index=False)
-
+'''
 #put the distribution of data for 'what is your 1st choice session?' in 'preference responses' into a dictionary
 #key is the session name, value is the number of students in that session
 session_distribution_1_1 = {}
@@ -109,6 +111,7 @@ for i in range(len(preference_responses)):
         session_distribution_1_5[preference_responses['What is your 5th choice session?'][i]] = 1
 #print(session_distribution_1_5)
 '''
+'''
 # put the 'max cap' of each session into a dictionary
 # key is the session name, value is the max cap of that session
 max_cap = {}
@@ -151,4 +154,104 @@ for i in range(len(all_students)):
         student.choices = [class1, class2,class3,class4]
 
     students.append(student)
-print(students)
+
+def find_program(id):
+    for p in programs:
+        if p.program_id == id:
+            return p
+
+    return None
+
+for i in programs:
+    print(i.program_id, len(i.students)) # print each program and how many students are in the program
+
+extra_students = []
+no_left = 0
+for student in students: # goes through every student
+    if student.presenter == True: #if presenter, pass
+        #print(student.email, "is presenter")
+        pass
+    elif len(student.choices) == 0: # if didnt make choices, then put him in the 'extra students' container
+        extra_students.append(student)
+    else:
+        print(student.choices[0]) # print the choices of the student
+        for pr_id in student.choices[0]: # for every program id, go through every choice that the student has
+            program = find_program(pr_id)
+            if program.is_available(): # checks if a program is available
+                program.students.append(student.email) # append the email to the list of students attending the program
+                student.assigned[0] = True # change the status of the student
+                break
+        if student.assigned[0] == False: # if none of the 5 choices are available, put the student into the 'extra students' container
+            extra_students.append(student)
+    no_left += 1
+#print("no left: ", no_left)
+all_diff = 0
+for p in range(0,21): # go through every program (put 0-21) because there are 21 programs in the first category
+    program = programs[p]
+    difference = 0
+    if len(program.students) < program.min_cap:
+        difference = program.min_cap - len(program.students) # difference between the minimum cap and the number of students in there (so we can see how many students need to be added to satisfy the min cap)
+        all_diff += difference # see how many people we need
+#print("Need people: ", all_diff)
+
+y = 0
+z = 0
+o = 0
+for x in students: # get the distribution of presenters, 
+    if x.presenter == True:
+        y+=1
+    if len(x.choices) == 0:
+        z+=1
+    #print(x.choices)
+    if x.presenter == True and len(x.choices) > 0:
+        o += 1
+#print("presenters: ",y)
+#print("no choices: ",z)
+#print("presenter with choices: ",o)
+#print(len(extra_students))
+
+#print(len(students))
+#for i in programs:
+#    print(i.program_id, len(i.students))
+
+test_list = []
+distribution = {}
+for student in students: # prints the dictionary of distribution of the choices 
+    if student.presenter == True:
+        #print(student.email, "is presenter")
+        pass
+    elif len(student.choices) == 0:
+        test_list.append(student)
+    else:
+        for choice in student.choices[0]:
+            if choice in distribution:
+                distribution[choice] += 1
+            else:
+                distribution[choice] = 1
+print(distribution)
+'''
+extra_students_test = []
+for student in students:
+    if student.presenter == True:
+        #print(student.email, "is presenter")
+        pass
+    elif len(student.choices) == 0:
+        extra_students_test.append(student)
+    else:
+        final_id = 0
+        cap = 999
+        for choice in student.choices[0]:
+            prog_capa = len(find_program(choice).students)
+            if find_program(choice).is_available() == True:
+                if prog_capa < cap:
+                    cap = prog_capa
+                    final_id = choice
+            find_program(final_id).students.append(student.email)
+        #print(student.email, "final ID: ", final_id)
+        if student.assigned[0] == False:
+            extra_students.append(student)
+
+#for i in programs:
+#    print(i.program_id, len(i.students))
+'''
+
